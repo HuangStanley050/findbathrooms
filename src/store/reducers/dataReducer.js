946c934656c2be0e4adce34db23eb4d0314f8194
Stanley Huang <huang.stanley050@gmail.com>
2019-05-15 16:19:31 +1000

    start to set up reducer

---
+++ src/store/reducers/dataReducer.js
@@ -0,0 +1,11 @@
+import * as actionType from "../actions/actionTypes";
+const initialState = {};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
