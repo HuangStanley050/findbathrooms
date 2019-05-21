91bea5fe3ebf063628907f9995b19aa70b1af870
Stanley Huang <huang.stanley050@gmail.com>
2019-05-21 21:13:11 +1000

    able to style the AlgoliaPlaces component

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -26,39 +26,41 @@ const SearchBar = props => {
   return (
     <section style={{marginTop: "20%"}}>
       <h1 className="text-center text-info mb-3">Search toilets anywhere</h1>
-      <AlgoliaPlaces
-        options={algoliaConfig}
-        onChange={({query, rawAnswer, suggestion, suggestionIndex}) => {
-          console.log(
-            "Fired when suggestion selected in the dropdown or hint was validated.",
-            suggestion
-          );
-          getLat(suggestion.latlng.lat);
-          getLong(suggestion.latlng.lng);
-        }}
-        onSuggestions={({rawAnswer, query, suggestions}) =>
-          console.log(
-            "Fired when dropdown receives suggestions. You will receive the array of suggestions that are displayed."
-          )
-        }
-        onCursorChanged={({rawAnswer, query, suggestion, suggestonIndex}) =>
-          console.log(
-            "Fired when arrows keys are used to navigate suggestions."
-          )
-        }
-        onClear={() => {
-          getLat("");
-          getLong("");
-        }}
-        onLimit={({message}) =>
-          console.log("Fired when you reached your current rate limit.")
-        }
-        onError={({message}) =>
-          console.log(
-            "Fired when we could not make the request to Algolia Places servers for any reason but reaching your rate limit."
-          )
-        }
-      />
+      <div style={{width: "80%", margin: "0 auto"}}>
+        <AlgoliaPlaces
+          options={algoliaConfig}
+          onChange={({query, rawAnswer, suggestion, suggestionIndex}) => {
+            console.log(
+              "Fired when suggestion selected in the dropdown or hint was validated.",
+              suggestion
+            );
+            getLat(suggestion.latlng.lat);
+            getLong(suggestion.latlng.lng);
+          }}
+          onSuggestions={({rawAnswer, query, suggestions}) =>
+            console.log(
+              "Fired when dropdown receives suggestions. You will receive the array of suggestions that are displayed."
+            )
+          }
+          onCursorChanged={({rawAnswer, query, suggestion, suggestonIndex}) =>
+            console.log(
+              "Fired when arrows keys are used to navigate suggestions."
+            )
+          }
+          onClear={() => {
+            getLat("");
+            getLong("");
+          }}
+          onLimit={({message}) =>
+            console.log("Fired when you reached your current rate limit.")
+          }
+          onError={({message}) =>
+            console.log(
+              "Fired when we could not make the request to Algolia Places servers for any reason but reaching your rate limit."
+            )
+          }
+        />
+      </div>
       <div className="text-center mt-4">
         <Button
           onClick={e => searchHandler()}
